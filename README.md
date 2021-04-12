9e68304c416efc3f9678041bd072ab3f48e37726
sungpah <admin@ringleplus.com>
2021-04-12 19:24:35 +0900

    update

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ https://docs.agora.io/en/Voice/start_call_audio_web_ng?platform=Web
 https://www.section.io/engineering-education/agora-express-token-server/
 
 
+https://github.com/AgoraIO-Community/AgoraWebSDK-NG/blob/master/Demo/basicLive/basicLive.js
+
 1. agora sdk 회원 가입
 2. agora sdk에서 프로젝트 형성 후, .env를 리액트 프로젝트에서 만든 다음
 REACT_APP_appId=somestringoverhere
--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import AgoraRTC from "agora-rtc-sdk-ng";
 import './App.css';
 // import ReactDOM from 'react-dom';
 
-//auth..!
-//접속을 하려면.. 
-
 var client = AgoraRTC.createClient({ mode: "rtc", codec: "h264" });
 var localTracks = {
   videoTrack: null,
@@ -90,7 +87,8 @@ function App() {
     await client.subscribe(user, mediaType);
     console.log("subscribe success");
     if (mediaType === 'video') {
-      
+      //uid - 
+
       if(uids.includes(uid)){
         //pass
       }else{
